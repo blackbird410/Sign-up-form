8543861650d35e35a1f53a4d58ebb70ab63447ee
Neil T. Rigaud <ntsrigaud14@gmail.com>
2024-01-21 14:19:55 +0800

    Added form validation

--- code.js
+++ code.js
@@ -1,14 +1,139 @@
 // Validate the form
-createLayout();
-addValidation();
-
-function addValidation() {
-	const form = document.querySelector('form');
-	form.setAttribute('noValidate', 'false');
-
-
+// I will add an event listener for every form input element and update a boolean variable everytime that the input is changed.
+// When all the input boolean variables are true, then the form can be allowed to be submitted.
+const regExp = {
+	'name': /^[A-Za-z]+([-']?[A-Za-z]+)*$/,
+	'email': /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+	'password': /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z0-9]{8,20}$/,
+	'phone': /[0-9]{3}[0-9]{3}[0-9]{4}/
 }
 
+createLayout();
+
+const myForm = document.querySelector('form');
+myForm.noValidate = true;
+
+let validInput = {
+	'first-name': false,
+	'last-name': false,
+	'email': false,
+	'phone': false,
+	'password': false,
+	'confirm-password': false,
+}
+addEvents();
+myForm.addEventListener('submit', validateForm);
+
+function validateForm(e) {
+	if (!(validInput['first-name'] && validInput['last-name'] 
+		&& validInput.email && validInput.phone 
+		&& validInput.password && validInput['confirm-password']))
+		e.preventDefault();
+}
+
+function addEvents() {
+	const inputs = document.querySelectorAll('input');
+	let exp;
+
+	Array.from(inputs).forEach(input => {
+		input.addEventListener('input', () => {
+			switch(input.id)
+			{
+				case 'first-name':
+					if (input.value.match(regExp.name))
+                                	{
+						input.parentElement.classList.remove('invalid');
+                                       	 	input.parentElement.classList.add('valid');
+                                        	validInput['first-name'] = true;
+                                	}
+                               	 	else
+                                	{
+                                        	input.parentElement.classList.remove('valid');
+                                        	input.parentElement.classList.add('invalid');
+                                        	validInput['first-name'] = false;
+                               	 	}
+                                	break;
+				case 'last-name':
+					if (input.value.match(regExp.name))
+                                	{
+                                        	input.parentElement.classList.remove('invalid');
+                                        	input.parentElement.classList.add('valid');
+                                        	validInput['last-name'] = true;
+                                	}
+                                	else
+                                	{
+                                        	input.parentElement.classList.remove('valid');
+                                        	input.parentElement.classList.add('invalid');
+                                        	validInput['last-name'] = false;
+                                	}
+                               		break;
+				case 'email':
+					if (input.value.match(regExp.email)) 
+					{
+						input.parentElement.classList.remove('invalid');
+						input.parentElement.classList.add('valid');
+						validInput.email = true;
+					}
+					else 
+					{
+						input.parentElement.classList.remove('valid');
+						input.parentElement.classList.add('invalid');
+						validInput.email = false;
+					}
+					break;
+				case 'phone-number':
+					if (input.value.match(regExp.phone))
+					{
+						input.parentElement.classList.remove('invalid');
+						input.parentElement.classList.add('valid');
+						validInput.phone = true;
+					}
+					else
+					{
+						input.parentElement.classList.remove('valid');
+						input.parentElement.classList.add('invalid');
+						validInput.phone = false;
+					}
+					break;
+				case 'password':
+					if (input.value.match(regExp.password))
+                                	{
+                                        	input.parentElement.classList.remove('invalid');
+                                        	input.parentElement.classList.add('valid');
+                                        	validInput.password = true;
+                                	}
+                                	else
+                                	{
+                                        	input.parentElement.classList.remove('valid');
+                                        	input.parentElement.classList.add('invalid');
+                                        	validInput.password = false;
+                                	}
+                                	break;
+				case 'confirm-password':
+					if (input.value === document.querySelector('#password').value && validInput.password)
+					{
+						input.parentElement.classList.remove('invalid');
+						input.parentElement.classList.add('valid');
+						validInput['confirm-password'] = true;
+					}
+					else
+					{
+						input.parentElement.classList.remove('valid');
+						input.parentElement.classList.add('invalid');
+						validInput['confirm-password'] = false;
+					}
+
+					if (validInput.password != validInput['confirm-password'])
+						input.parentElement.classList.add('not-match');
+					else
+						input.parentElement.classList.remove('not-match');
+					break;
+				default:
+					console.log('Error');
+					break;
+			}});
+	});
+}
 
 function createLayout() {
 	const body = document.body;
@@ -73,8 +198,10 @@ function createLayout() {
 		const container = document.createElement('div');
 		const label = document.createElement('label');
 		const input = document.createElement('input');
+		const help = document.createElement('span');
 
 		container.classList.add('container', 'input-container');
+		help.classList.add('help');
 
 		let id = getId(field);
 		label.setAttribute('for', id);
@@ -86,10 +213,12 @@ function createLayout() {
 				input.setAttribute('type', 'email');
 				input.setAttribute('placeholder', 'alanturing@gmail.com');
 				input.setAttribute('autocomplete', 'on');
+				help.textContent = "Please enter a valid email address";
 				break;
 			case 'PHONE NUMBER':
 				input.setAttribute('type', 'tel');
 				input.setAttribute('placeholder', '0937549257');
+				help.textContent = "Please enter a valid phone number";
 				break;
 			case 'PASSWORD':
 			case 'CONFIRM PASSWORD':
@@ -98,22 +227,28 @@ function createLayout() {
 					input.setAttribute('placeholder', 'mathison@cs1900');
 				else
 					input.setAttribute('placeholder', 'mathison@cs1900');
+				help.textContent = "Please enter a valid password";
 				break;
 			default:
 				input.setAttribute('type', 'text');
 				input.setAttribute('autocomplete', 'on');
+				input.setAttribute('minlength', '2');
+				input.setAttribute('maxlength', '50');
 				if (field == "FIRST NAME")
 					input.setAttribute('placeholder', 'Alan');
 				else
 					input.setAttribute('placeholder', 'Turing');
+				help.textContent = "Please enter a valid name";
 				break;
 		}
 
 		input.setAttribute('id', id);
 		input.setAttribute('name', id);
+		input.required = true;
 
 		container.appendChild(label);
 		container.appendChild(input);
+		container.appendChild(help);
 		fieldset.appendChild(container);
 	});
 
